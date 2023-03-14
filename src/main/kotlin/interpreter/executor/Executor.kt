4e3346a214f8fde8cba12b54af5bbdd2f04dcb81
Juan Cruz Estevez <72108526+juancruzestevez@users.noreply.github.com>
2023-03-13 22:51:27 -0300

    executor algo hecho

--- src/main/kotlin/interpreter/executor/Executor.kt
+++ src/main/kotlin/interpreter/executor/Executor.kt
@@ -9,16 +9,33 @@ data class IntValue(val value: Int): Binary{
 data class StringValue(val value: String): Binary{
     override fun evaluate(): Any = value
 }
-data class BinaryExprecion(val left: Binary, val right: Binary, val op: Operator): Binary{
+data class BinaryOperation(val left: Binary, val right: Binary, val op: Operator): Binary{
     override fun evaluate(): Any {
         val leftValue = left.evaluate()
         val rightValue = right.evaluate()
         return when {
-            leftValue is String && rightValue is String -> leftValue + rightValue
-            leftValue is Int && rightValue is Int -> leftValue + rightValue
-            leftValue is String && rightValue is Int -> leftValue + rightValue.toString()
-            leftValue is Int && rightValue is String -> leftValue.toString() + rightValue
+            leftValue is String && rightValue is String -> operationString(leftValue, rightValue, op)
+            leftValue is Int && rightValue is Int -> operation(leftValue, rightValue, op)
+            leftValue is String && rightValue is Int -> operationString(leftValue, rightValue.toString(), op)
+            leftValue is Int && rightValue is String -> operationString(leftValue.toString(), rightValue, op)
             else -> throw IllegalArgumentException("Tipos incompatibles: $leftValue, $rightValue")
+        }
+    }
+
+    fun operation(left: Int, right: Int, op: Operator) : Int{
+        return when{
+            op == Operator.PLUS -> left + right
+            op == Operator.MINUS -> left - right
+            op == Operator.PRODUCT -> left * right
+            op == Operator.DIVISION -> left / right
+            else -> throw IllegalArgumentException("Error")
+        }
+    }
+
+    fun operationString(left: String, right: String, op: Operator) : String{
+        return when{
+            op == Operator.PLUS -> left + right
+            else -> throw IllegalArgumentException("Error")
         }
     }
 
